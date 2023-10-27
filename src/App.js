18d06f13d82ca7867a5b0bbc10345043e48fd70b
shlokaskshetty <122727116+shlokaskshetty@users.noreply.github.com>
2023-10-27 18:38:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { pkg } from '@carbon/ibm-products';
+import './App.css';
+import './app.scss';
+import React, { Component } from 'react';
+import { Button, Content } from '@carbon/react';
+import CarbonHeader from './components/header';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './content/dashboard';
+import Home from './content/home';
+import Login from './content/login';
+import EmpPage from './content/empPage';
+import '@carbon/ibm-products/css/index.min.css';
+
+pkg.setAllComponents(true);
+pkg.setAllFeatures(true);
+
+class App extends Component {
+  render() {
+    return (
+  <Content>
+  <Routes>
+  <Route exact path="/" element={<Login />} />
+      <Route path="/home" element={<div><Home /><CarbonHeader /></div>} />
+      <Route exact path="/emppage" element={<div><EmpPage /><CarbonHeader /></div>} />
+      <Route exact path="/dashboard" element={<div><Dashboard /><CarbonHeader /></div>} />
+  </Routes>
+  </Content>
+    );
+  }
+}
+
+export default App;
+
